7138065a4ab597cee7ab0653d61d6597d8ca966e
Diarit-S <diarit-94@hotmail.fr>
2019-03-17 19:53:34 +0100

    Jiren almost finished

--- src/Jiren/main.js
+++ src/Jiren/main.js
@@ -1,5 +1,10 @@
 var liElts = document.querySelectorAll('ul li');
 var articleElts = document.querySelectorAll('article');
+var h2Elts = document.querySelectorAll('aside h2');
+var pElts = document.querySelectorAll('aside p');
+var imgElts = document.querySelectorAll('aside img');
+
+
 
 liElts.forEach(li => {
   li.addEventListener('click', ()=>{
@@ -11,12 +16,21 @@ liElts.forEach(li => {
   })
 });
 
-for (let i = 0; i < liElts.length; i++) {
+for (let i = 0; i < 3; i++) {
   liElts[i].addEventListener('click', ()=>{
-    articleElts.forEach(article => {
-      article.classList.add('moved');
-    })
+
+    for (let j = 0; j < 3; j++) {
+      articleElts[j].classList.add('moved');
+      h2Elts[j].classList.add('is-up');
+      pElts[j].classList.add('is-right');
+      imgElts[j].classList.add('is-btm');
+
+    }
     articleElts[i].classList.remove('moved');
+    h2Elts[i].classList.remove('is-up');
+    pElts[i].classList.remove('is-right');
+    imgElts[i].classList.remove('is-btm');
+
   })
 }
 
